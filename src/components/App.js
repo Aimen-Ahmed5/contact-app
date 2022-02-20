c790085040b24a6b146e1ab4e69ceb01e8635df5
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-20 15:08:51 +0500

    passing function as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  
+         //object ( when usestate is not used )
+  // const add_contact = [
+  //   {
+  //     id: "1",
+  //     name: "John",
+  //     email: "johngreen282@gmail.com"
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Baldwin",
+  //     email: "baldwinkim582@gmail.com"
+  //   } ];
 
-  const add_contact = [
-    {
-      id: "1",
-      name: "John",
-      email: "johngreen282@gmail.com"
-    },
-    {
-      id: "2",
-      name: "Baldwin",
-      email: "baldwinkim582@gmail.com"
-    }
-  ];
+  // initially empty array for contacts
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+
+    console.log(contact);
+  }
 
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts = {add_contact}/> 
+      <AddContact addContactHandler = {addContactHandler}/>
+      {/* <ContactList contacts = {add_contact}/>  */}
    </div>
   );
 }
