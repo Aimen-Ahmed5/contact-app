bf7d936124fc4ad44dfe98a5189cde722ff34af6
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-20 17:02:33 +0500

    useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -19,19 +19,31 @@ function App() {
   //     email: "baldwinkim582@gmail.com"
   //   } ];
 
+  const LOCAL_STORAGE_KEY = "contacts";
+
   // initially empty array for contacts
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
 
     console.log(contact);
-  }
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    const retreiveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retreiveContacts) setContacts(retreiveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler = {addContactHandler}/>
-      {/* <ContactList contacts = {add_contact}/>  */}
+      <ContactList contacts = {contacts}/>
    </div>
   );
 }
