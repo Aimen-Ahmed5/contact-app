841a98e88027683f9ffcb35ca08376e92305745f
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-20 09:01:11 +0500

    interface of contact manager application is created

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+
+        // Semantic UI classes used
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
