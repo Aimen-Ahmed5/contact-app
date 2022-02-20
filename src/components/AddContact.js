4df7f32bdd0a65bda3e8b9d6d2ce4fa3652dabc8
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-20 15:09:24 +0500

    events and handlers ussed to pass data

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -2,17 +2,35 @@ import React from 'react';
 
 const AddContact = () => {
 
+   const state = {
+        name: "",
+        email: ""
+    }
+
+   const add = (event) => {
+        event.preventDefault();
+        if(this.state.name === "" && this.state.email === ""){
+            alert("All fields are mandatory");
+            return;
+        }
+
+        this.props.addContactHandler(this.state);
+        console.log(this.state);
+    }
+
     return (
         <div className="ui main">
-            <h2>Add Contact</h2>
-            <form className="ui form">
+            <h3>Add Contact</h3>
+            <form className="ui form" onSubmit= {(this.add) }>
                 <div className="field">
                     <label>Name</label>
-                    <input type="text" name="name" placeholder="Name" />
+                    <input type="text" name="name" placeholder="Name" value={this.state.name} 
+                    onChange={ (event) => this.setState({name: event.target.value}) }/>
                 </div>
                 <div className="field">
                     <label>Email</label>
-                    <input type="text" name="email" placeholder="Email" />
+                    <input type="text" name="email" placeholder="Email" value={this.state.email} 
+                    onChange={ (event) => this.setState({email: event.target.value}) } />
                 </div>
                 <button className="ui button blue">Add</button>
             </form>
