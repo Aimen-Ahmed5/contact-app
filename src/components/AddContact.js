3a5178a549a18e62ecc075b25e002adc1c097ac7
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-20 17:01:48 +0500

    class component, handler and object used

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,41 +1,45 @@
 import React from 'react';
 
-const AddContact = () => {
+class AddContact extends React.Component {
 
-   const state = {
+    state = {
         name: "",
         email: ""
-    }
+    };
 
-   const add = (event) => {
-        event.preventDefault();
-        if(this.state.name === "" && this.state.email === ""){
+    add = (e) => {
+        e.preventDefault();
+        if(this.state.name === "" || this.state.email === ""){
             alert("All fields are mandatory");
             return;
         }
 
         this.props.addContactHandler(this.state);
-        console.log(this.state);
-    }
+        //clearing name and email
+        this.setState({name: "", email: ""});
+     
+    };
+
+    render() {
 
     return (
         <div className="ui main">
             <h3>Add Contact</h3>
-            <form className="ui form" onSubmit= {(this.add) }>
+            <form className="ui form" onSubmit= {this.add}>
                 <div className="field">
                     <label>Name</label>
                     <input type="text" name="name" placeholder="Name" value={this.state.name} 
-                    onChange={ (event) => this.setState({name: event.target.value}) }/>
+                    onChange={ (e) => this.setState({name: e.target.value}) }/>
                 </div>
                 <div className="field">
                     <label>Email</label>
                     <input type="text" name="email" placeholder="Email" value={this.state.email} 
-                    onChange={ (event) => this.setState({email: event.target.value}) } />
+                    onChange={ (e) => this.setState({email: e.target.value}) } />
                 </div>
                 <button className="ui button blue">Add</button>
             </form>
         </div>
     );
 }
-
+}
 export default AddContact;
